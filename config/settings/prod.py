25866cbfb12f103ba210d2935abb2216e95b7ed6
김현우 <lackdoy@naver.com>
2022-04-14 13:05:33 +0900

    STATIC_ROOT create

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['172.30.1.21', 'testbbiome.tk']
-#ALLOWED_HOSTS = ['172.30.1.21']
+#ALLOWED_HOSTS = ['172.30.1.21', 'testbbiome.tk']
+ALLOWED_HOSTS = ['172.30.1.21']
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
