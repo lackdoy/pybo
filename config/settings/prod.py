ecbb6ac14aedaadc40b3bc935284250267aa0932
김현우 <oing117@gmail.com>
2022-04-18 09:55:16 +0900

    settings 파일 중 prod.py STATIC_ROOT 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,7 +1,8 @@
 from .base import *
+import os
 
 #ALLOWED_HOSTS = ['172.30.1.21', 'testbbiome.tk']
 ALLOWED_HOSTS = ['172.30.1.21']
-STATIC_ROOT = BASE_DIR / 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = []
 # concept addon
