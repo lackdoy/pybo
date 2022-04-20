2bee2e3b4f3d7b64f077ab3717963fb2411e484a
김현우 <oing117@gmail.com>
2022-04-20 14:18:06 +0900

    debuf setting turn to False

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,6 +4,6 @@ import os
 #ALLOWED_HOSTS = ['172.30.1.21', 'testbbiome.tk']
 ALLOWED_HOSTS = ['172.30.1.21']
 STATIC_ROOT = BASE_DIR / 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = []
+DEBUG = False
 # concept addon
