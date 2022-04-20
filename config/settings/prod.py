8089d774026084df33f7c9329b113afc96610bf7
김현우 <oing117@gmail.com>
2022-04-20 14:00:49 +0900

    settings prod.py 수정

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -3,7 +3,7 @@ import os
 
 #ALLOWED_HOSTS = ['172.30.1.21', 'testbbiome.tk']
 ALLOWED_HOSTS = ['172.30.1.21']
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static/'
 #STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = []
 # concept addon
